1e3bf05ab040b3f915eb33845df3539b83c4e6e9
Anton Kechashin <atk@sitecore.net>
2017-06-23 14:10:35 +0300

    Initial commit.

---
+++ src/Sitecore.Support.169679/Shell/Applications/ContentEditor/Date.cs
@@ -0,0 +1,14 @@
+﻿namespace Sitecore.Support.Shell.Applications.ContentEditor
+{
+  using System;
+  public class Date : Sitecore.Shell.Applications.ContentEditor.Date
+  {
+    protected override void OnInit(EventArgs e)
+    {
+      #region Added code
+      this.Disabled = this.ReadOnly; 
+      #endregion
+      base.OnInit(e);
+    }
+  }
+}
---
+++ src/Sitecore.Support.169679/Shell/Applications/ContentEditor/DateTime.cs
@@ -0,0 +1,14 @@
+﻿namespace Sitecore.Support.Shell.Applications.ContentEditor
+{
+  using System;
+  public class DateTime : Sitecore.Shell.Applications.ContentEditor.DateTime
+  {
+    protected override void OnInit(EventArgs e)
+    {
+      #region Added code
+      this.Disabled = this.ReadOnly; 
+      #endregion
+      base.OnInit(e);
+    }
+  }
+}
